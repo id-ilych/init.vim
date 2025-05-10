3fe3568d38912cc111ac6aff8d5719b793c54d01
Ilya Denisov <id.ilych@gmail.com>
2025-05-10 17:00:38 +0300

    use make for telescope-fzf-native
    the build manifest is not compatible with the recent version of cmake

--- init.lua
+++ init.lua
@@ -74,7 +74,7 @@ require("lazy").setup({
   'jparise/vim-graphql',
   'tpope/vim-endwise',
 
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   'rafi/awesome-vim-colorschemes',

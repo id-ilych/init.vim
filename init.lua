0c0bfcede86e92c98c545cbbb9c17a41e8c9f62c
Ilia Denisov <id.ilych@gmail.com>
2025-06-21 00:34:03 +0300

    add hotkeys to cycle through color schemes

--- init.lua
+++ init.lua
@@ -179,4 +179,42 @@ vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
   replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
+-- Cycle through color schemes
+local colorschemes = vim.fn.getcompletion("", "color")
+local colorschemes_idx = vim.fn.index(
+    colorschemes,
+    vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
+)
+
+local function change_colorscheme(forward)
+    if forward then
+        colorschemes_idx = colorschemes_idx + 1
+    else
+        colorschemes_idx = colorschemes_idx - 1
+    end
+
+    if colorschemes_idx > #colorschemes then
+        colorschemes_idx = 1
+    elseif colorschemes_idx < 1 then
+        colorschemes_idx = #colorschemes
+    end
+
+    local ok = pcall(function()
+        vim.cmd("colorscheme " .. colorschemes[colorschemes_idx])
+    end)
+
+    if not ok then
+        change_colorscheme(forward)
+    end
+
+    print(colorschemes[colorschemes_idx])
+end
+
+vim.keymap.set("n", "<C-h>", function()
+    change_colorscheme(true)
+end)
+
+vim.keymap.set("n", "<C-l>", function()
+    change_colorscheme(false)
+end)
 

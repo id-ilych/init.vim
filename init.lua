7a555a17813c96009251b3774ed936cc6dfac41c
Ilia Denisov <id.ilych@gmail.com>
2025-06-21 00:33:35 +0300

    change tabstop 2 -> 4

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ vim.o.smartindent = true
 vim.o.smarttab = true
 vim.o.expandtab = true
 vim.o.softtabstop = 2
-vim.o.tabstop = 2
+vim.o.tabstop = 4
 vim.o.shiftwidth = 2
 
 vim.o.fileencodings = 'utf-8,cp1251'  -- try these when opening wiles
